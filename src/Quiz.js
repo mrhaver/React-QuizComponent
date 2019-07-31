ac6147de4bb2c2ad6631de97d33ed67020c524c1
Frank Haver <frank@marvelous.nl>
2019-07-31 14:33:48 +0200

    Added click events

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,12 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
         return (
@@ -21,7 +27,7 @@ class Quiz extends Component {
                 {isQuizEnd ?
                     <QuizEnd />
                     :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
             </div>
         )
